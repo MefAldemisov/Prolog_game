ff77230cc43d600a567c898885fe918fe2e03f84
MefAldemisov <alisha-omg@yandex.ru>
2020-03-02 20:52:58 +0300

    Excessive methods removed

--- README.md
+++ README.md
@@ -2,4 +2,15 @@
 
 1. Random search
 2. Backtracking
-3. Some other algorithm (A*...)
+3. Some other algorithm (A*, Simulated Annealing)
+
+## TODO:
+
+- [x] Remove excessive methods from files
+- [ ] See the touchdown point
+- [ ] Show time meric only
+- [ ] Change region of seeeing
+- [ ] Scale the system
+- [ ] Add 10 tests
+- [ ] Add unslvable test and test with touchdown (0, 0)
+
--- main.pl
+++ main.pl
@@ -5,10 +5,21 @@
 % t(X, Y).
 % orc(X, Y).
 % human(X, Y).
-% t(1, 2).
-% orc(2, 1).
+t(1, 2).
+orc(2, 1).
 correct.
-% human(2, 4).
+human(2, 4).
+
+human(1, 9).
+human(3, 0).
+human(0, 2).
+human(0, 3).
+human(5, 5).
+human(9, 9).
+human(7, 7).
+human(0, 7).
+human(2, 7).
+human(3, 6).
 
 % human(1, 9).
 % human(3, 0).
@@ -28,8 +39,8 @@ correct.
 clone_list([],[]).
 clone_list([H|T],[H|Z]):- clone_list(T,Z).
 
-check_border(1, X, Y, X_new, Y_new) :- Y_new is Y + 1, Y_new < 20, X_new is X.
-check_border(2, X, Y, X_new, Y_new) :- X_new is X + 1, X_new < 20, Y_new is Y.
+check_border(1, X, Y, X_new, Y_new) :- Y_new is Y + 1, Y_new < 10, X_new is X.
+check_border(2, X, Y, X_new, Y_new) :- X_new is X + 1, X_new < 10, Y_new is Y.
 check_border(3, X, Y, X_new, Y_new) :- Y_new is Y - 1, Y_new >= 0, X_new is X.
 check_border(4, X, Y, X_new, Y_new) :- X_new is X - 1, X_new >= 0, Y_new is Y.
 
@@ -43,13 +54,13 @@ look_around(Step, X, Y, Check_orcs) :-
     check_border(Step, X, Y, X_new, Y_new),
     (Check_orcs is 1 -> no_orc(X_new, Y_new); correct).
 
-test_look_around :-
-    look_around(1, 0, 0, 1), look_around(2, 0, 0, 1), \+ look_around(3, 0, 0, 1), \+ look_around(4, 0, 0, 1),  
-    \+look_around(1, 0, 19, 1), look_around(2, 0, 19, 1), look_around(3, 0, 19, 1), \+look_around(4, 0, 19, 1),
-    look_around(1, 19, 0, 1), \+look_around(2, 19, 0, 1), \+look_around(3, 19, 0, 1), look_around(4, 19, 0, 1),
-    \+ look_around(1, 19, 19, 1), \+ look_around(2, 19, 19, 1), look_around(3, 19, 19, 1), look_around(4, 19, 19, 1),
-    look_around(1, 1, 1, 1), \+ look_around(2, 1, 1, 1), look_around(3, 1, 1, 1), look_around(4, 1, 1, 1),  
-    look_around(1, 10, 4, 1), look_around(2, 10, 4, 1), look_around(3, 10, 4, 1), look_around(4, 10, 4, 1).
+% test_look_around :-
+%     look_around(1, 0, 0, 1), look_around(2, 0, 0, 1), \+ look_around(3, 0, 0, 1), \+ look_around(4, 0, 0, 1),  
+%     \+look_around(1, 0, 19, 1), look_around(2, 0, 19, 1), look_around(3, 0, 19, 1), \+look_around(4, 0, 19, 1),
+%     look_around(1, 19, 0, 1), \+look_around(2, 19, 0, 1), \+look_around(3, 19, 0, 1), look_around(4, 19, 0, 1),
+%     \+ look_around(1, 19, 19, 1), \+ look_around(2, 19, 19, 1), look_around(3, 19, 19, 1), look_around(4, 19, 19, 1),
+%     look_around(1, 1, 1, 1), \+ look_around(2, 1, 1, 1), look_around(3, 1, 1, 1), look_around(4, 1, 1, 1),  
+%     look_around(1, 10, 4, 1), look_around(2, 10, 4, 1), look_around(3, 10, 4, 1), look_around(4, 10, 4, 1).
 
 appropriate_pass_id(X, Y, 11) :- look_around(1, X, Y, 0).
 appropriate_pass_id(X, Y, 12) :- look_around(1, X, Y, 0), look_around(2, X, Y, 0).
@@ -72,21 +83,35 @@ generate_appropriate_pass_id(X, Y, P) :-
     findall(S, appropriate_pass_id(X, Y, S), L), 
     random_member(P, L).
 
-generate_appropriate_step_id(X, Y, Can_pass, Step_id) :-
+generate_random_appropriate_step_id(X, Y, Can_pass, Step_id) :-
     
     /**
      * (X, Y) - current coordinates
      * Can_pass is 1 -> can pass else - cannot
      * Step_id - return value
      */
-    
 	findall(S, look_around(S, 0, 0, 1), List_of_steps),
     (Can_pass is 1 -> append(List_of_steps, [5], List_of_all)),
     random_member(Step, List_of_all),
     (Step is 5 -> 
     	generate_appropriate_pass_id(X, Y, Step_id); 
     	Step_id is Step).
+
+generate_appropriate_step_id(X, Y, Can_pass, Step_id) :-
+
+    /**
+     * (X, Y) - current coordinates
+     * Can_pass is 1 -> can pass else - cannot
+     * Step_id - return value
+     */
+    findall(S, look_around(S, 0, 0, 1), List_of_steps),
+    (Can_pass is 1 -> append(List_of_steps, [5], List_of_all)),
+    member(Step, List_of_all),
+    (Step is 5 -> 
+        appropriate_pass_id(X, Y, Step_id); 
+        Step_id is Step).
     
+
 make_step(Step_id, X, Y, X_new, Y_new) :- check_border(Step_id, X, Y, X_new, Y_new).
 
 search_human(X, Y, Step_x, Step_y, X_new, Y_new) :-
@@ -99,7 +124,7 @@ search_human(X, Y, Step_x, Step_y, X_new, Y_new) :-
 
     X_n is X + Step_x,
     Y_n is Y + Step_y,
-    X_n >= 0, Y_n >= 0, X_n < 20, Y_n < 20, \+ orc(X_n, Y_n),
+    X_n >= 0, Y_n >= 0, X_n < 10, Y_n < 10, \+ orc(X_n, Y_n),
     (   human(X_n, Y_n) ->  
     (   X_new is X_n, 
     	Y_new is Y_n);
@@ -124,7 +149,7 @@ update_path(X_new, Y_new, Step_id, Path, New_path) :-
     append(Path, [[X_new, Y_new]], New_path);
     append(Path, [["P", X_new, Y_new]], New_path).
 
-find_path(X, Y, Can_pass, Path, Score, Total_path, Total_score) :-
+find_path(X, Y, Can_pass, Path, Score, Total_path, Total_score, Rand) :-
 
     /**
      * Searches randomly for the next step and make it
@@ -140,14 +165,18 @@ find_path(X, Y, Can_pass, Path, Score, Total_path, Total_score) :-
     t(X, Y) ->
         (clone_list(Path, Total_path), 
         Total_score is Score);
-    (generate_appropriate_step_id(X, Y, Can_pass, Step_id),
+    (
+    (Rand is 1 ->
+        generate_random_appropriate_step_id(X, Y, Can_pass, Step_id);
+        generate_appropriate_step_id(X, Y, Can_pass, Step_id)
+    ),
     % somethin else
     (Step_id > 4 -> make_pass(Step_id, X, Y, X_new, Y_new);
     make_step(Step_id, X, Y, X_new, Y_new)), 
     update_path(X_new, Y_new, Step_id, Path, New_path),
     (Step_id > 4 -> New_can_pass is 0; New_can_pass is Can_pass),
     (human(X_new, Y_new) -> New_score is Score; New_score is Score + 1),
-    find_path(X_new, Y_new, New_can_pass, New_path, New_score, Total_path, Total_score)). 
+    find_path(X_new, Y_new, New_can_pass, New_path, New_score, Total_path, Total_score, Rand)). 
 
 
 new_best_score(A, B, C, A_path, B_path, C_path) :- 
@@ -170,6 +199,25 @@ printlist([X|List]) :-
     print_list_item(X), nl,
     printlist(List).
 
+% find the list(path) + length which is the shortest
+list_min([[P, S]|Ls], Min) :-
+    list_min(Ls, S, Min).
+
+list_min([], Min, Min).
+list_min([[P, S]|Ls], Min0, Min) :-
+    Min1 is min(S, Min0),
+    list_min(Ls, Min1, Min).
+
+% find the shortest path
+get_path_with_min(P, M, []) :-
+    fail.
+get_path_with_min(P, M, [[A, B]|L]) :-
+    (   M is B ->   
+    	clone_list(P,A);
+    	get_path_with_min(P, M, L)).
+
+% ramdom search
+
 random_path(100, Score, Best, Path) :-
     format("\n\nTotal score is ~w", [Score]),
     writeln("\nNew path: " ), printlist(Path),
@@ -177,9 +225,8 @@ random_path(100, Score, Best, Path) :-
 
 random_path(Itteration, Score, Best, Path) :-
     Itteration < 100,
-    format("New ITTERATION ~d \n" , [Itteration]),
     Next_itr is Itteration + 1,
-    (find_path(0, 0, 1, [], 0, New_path, New_score)->
+    (find_path(0, 0, 1, [], 0, New_path, New_score, 1)->
             (new_best_score(Score, New_score, Nbs, Path, New_path, Best_path),
             random_path(Next_itr, Nbs, New_best, Best_path), Best is New_best);
         random_path(Next_itr, Score, New_best, Path), Best is New_best).
